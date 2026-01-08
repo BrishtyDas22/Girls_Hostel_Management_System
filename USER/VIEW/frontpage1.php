f5b25e9f9a997ded92ec8eb0701490f7793f9900
pkhunter47 <protikbiswas3099@gmail.com>
2026-01-09 04:26:24 +0600

    3

--- USER/VIEW/frontpage1.php
+++ USER/VIEW/frontpage1.php
@@ -13,7 +13,7 @@
     <h1>Welcome to Happy-Life</h1>
     </div>
     <div id="header_image_div">
-    <img src="../images/94f891a8a5192810c8dbef05ccef6a26.jpg" alt="header image" id="header_image">
+    <img src="../images/hostel-g.jpg" alt="header image" id="header_image">
     <h1 id="header_image_h1">Find your HOME!</h1>
     <p>Comfortable, Secure, and Affordable Student Accommodation</p>
     </div>
@@ -22,6 +22,7 @@
 
     <button type="submit" id="register" onclick="location.href='registration1.php'">Register Now</button>
     <button type="submit" id="login" onclick="location.href='Login.php'">Login</button>
+    <button type="submit" id="Alogin">Admin Login</button>
 
 
 
