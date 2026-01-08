f2f599cbabf5d615b862a072381ad9ec42547f3b
pkhunter47 <protikbiswas3099@gmail.com>
2026-01-09 03:10:44 +0600

    changes

--- USER/VIEW/frontpage1.php
+++ USER/VIEW/frontpage1.php
@@ -12,6 +12,8 @@
     </div>
     <div id="header_image_div">
     <img src="../images/94f891a8a5192810c8dbef05ccef6a26.jpg" alt="header image" id="header_image">
+    <h1 id="header_image_h1">Find your HOME!</h1>
+    <p>Comfortable, Secure, and Affordable Student Accommodation</p>
     </div>
 
 
