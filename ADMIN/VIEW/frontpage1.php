8014fe59785102caae860609d9e5ecb6f49373b1
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2026-01-14 23:35:26 +0600

    image name changes

--- ADMIN/VIEW/frontpage1.php
+++ ADMIN/VIEW/frontpage1.php
@@ -8,12 +8,12 @@
 <body>
     <div id="header_div">
 
-    <img src="../images/machine.jpeg" alt="logo" id="header_logo">
+    <img src="../IMAGES/machine.jpeg" alt="logo" id="header_logo">
        
     <h1>Welcome to Happy-Life</h1>
     </div>
     <div id="header_image_div">
-    <img src="../images/hostel-g.jpg" alt="header image" id="header_image">
+    <img src="../IMAGES/hostel-g.jpg" alt="header image" id="header_image">
     <h1 id="header_image_h1">Find your HOME!</h1>
     <p>Comfortable, Secure, and Affordable Student Accommodation</p>
     </div>
@@ -32,7 +32,7 @@
      <div id="room_box">
 
 
-    <img src="../images/2bed.jpg" alt="room image " id="room_image"> 
+    <img src="../IMAGES/2bed.jpg" alt="room image " id="room_image"> 
 
     <div class="room_info"><h3>Room Info:</h3>
         <p class="room_no">Room No:101 </p>
@@ -45,7 +45,7 @@
      <div id="room_box1">
 
 
-    <img src="../images/1.jpg" alt="room image" id="room_image">
+    <img src="../IMAGES/1.jpg" alt="room image" id="room_image">
 
     <div class="room_info1"><h3>Room Info:</h3>
         <p class="room_no">Room No:102 </p>
@@ -57,7 +57,7 @@
 
         <div id="room_box2">
 
-    <img src="../images/img3.jpg" alt="room image" id="room_image">
+    <img src="../IMAGES/img3.jpg" alt="room image" id="room_image">
     <div class="room_info2"><h3>Room Info:</h3>
         <p class="room_no">Room No:103 </p>
     <p class="price">Price: 8000tk/month</p>
@@ -68,7 +68,7 @@
 
          <div id="room_box3">
 
-    <img src="../images/img4.jpg" alt="room image" id="room_image">
+    <img src="../IMAGES/img4.jpg" alt="room image" id="room_image">
     <div class="room_info3"><h3>Room Info:</h3>
         <p class="room_no">Room No:104 </p>
     <p class="price">Price: 12000tk/month</p>
@@ -79,7 +79,7 @@
 
 
         <div id="room_box4">
-    <img src="../images/img5.png" alt="room image" id="room_image">
+    <img src="../IMAGES/img5.png" alt="room image" id="room_image">
     <div class="room_info4"><h3>Room Info:</h3>
         <p class="room_no">Room No:105 </p>
     <p class="price">Price: 9000tk/month</p>
@@ -88,7 +88,7 @@
      </div> </div> 
 
         <div id="room_box5">
-    <img src="../images/3bed.avif" alt="room image" id="room_image">
+    <img src="../IMAGES/3bed.avif" alt="room image" id="room_image">
     <div class="room_info5"><h3>Room Info:</h3>
         <p class="room_no">Room No:106 </p>
     <p class="price">Price: 11000tk/month</p>
