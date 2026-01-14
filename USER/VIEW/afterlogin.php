aa17f5dbc92b73a6b1878c92d0c4151b57388534
pkhunter47 <protikbiswas3099@gmail.com>
2026-01-15 01:35:59 +0600

    work loading

--- USER/VIEW/afterlogin.php
+++ USER/VIEW/afterlogin.php
@@ -33,6 +33,80 @@ if (!isset($_SESSION["username"])) {
             <label id="r1">Room No:</label>
             <label id="p1">Price:</label>
             <p id ="r1_info"><b><i>Fully furnished with cleaning services provided twice a week. Includes high-speed internet and access to the common room.</i></b></p>
+            <button type="submit" id="book_now1">Book Now!</button>
+
+        </form>
+
+
+    </div>
+
+    <div id="room_info2">
+        <form method="post" id="booking-form2">
+            <img src="../images/1.jpg" alt="room2.png" id="room2_img">
+            <h3>Room Info:</h3>
+            <label id="r2">Room No:</label>
+            <label id="p2">Price:</label>
+            <p id ="r2_info"><b><i>A comfortable, ready-to-occupy room with scheduled cleaning services, reliable high-speed internet, and access to shared common areas.</i></b></p>
+            <button type="submit" id="book_now2">Book Now!</button>
+
+        </form>
+
+
+    </div>
+
+
+    <div id="room_info3">
+        <form method="post" id="booking-form3">
+            <img src="../images/img3.jpg" alt="room3.png" id="room3_img">
+            <h3>Room Info:</h3>
+            <label id="r3">Room No:</label>
+            <label id="p3">Price:</label>
+            <p id ="r3_info"><b><i>Well-prepared living space with biweekly cleaning, dependable internet connectivity, and shared common room privileges with air conditioning.</i></b></p>
+            <button type="submit" id="book_now3">Book Now!</button>
+
+        </form>
+
+
+    </div>
+
+
+    <div id="room_info4">
+        <form method="post" id="booking-form4">
+            <img src="../images/img4.jpg" alt="room4.png" id="room4_img">
+            <h3>Room Info:</h3>
+            <label id="r4">Room No:</label>
+            <label id="p4">Price:</label>
+            <p id ="r4_info"><b><i>Comfortable living space offered with regular twice-weekly cleaning, fast and reliable internet, and use of shared communal areas.</i></b></p>
+            <button type="submit" id="book_now4">Book Now!</button>
+
+        </form>
+
+
+    </div>
+
+    <div id="room_info5">
+        <form method="post" id="booking-form5">
+            <img src="../images/img5.png" alt="room5.png" id="room5_img">
+            <h3>Room Info:</h3>
+            <label id="r5">Room No:</label>
+            <label id="p5">Price:</label>
+            <p id ="r5_info"><b><i>Comfortable living space offered with regular twice-weekly cleaning, fast and reliable internet, and use of shared communal areas.</i></b></p>
+            <button type="submit" id="book_now5">Book Now!</button>
+
+        </form>
+
+
+    </div>
+
+    <div id="room_info6">
+        <form method="post" id="booking-form6">
+            <img src="../images/3bed.avif" alt="room6.png" id="room6_img">
+            <h3>Room Info:</h3>
+            <label id="r6">Room No:</label>
+            <label id="p6">Price:</label>
+            <p id ="r6_info"><b><i>Comfortable living space offered with regular twice-weekly cleaning, fast and reliable internet, and use of shared communal areas.</i></b></p>
+            <button type="submit" id="book_now6">Book Now!</button>
+
         </form>
 
 
