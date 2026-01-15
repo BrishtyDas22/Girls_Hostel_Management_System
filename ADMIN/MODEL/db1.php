9df7bd20d40eb9f02fdabda77b4a9a3bc0c7d09d
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2026-01-16 05:10:00 +0600

    done 2

--- ADMIN/MODEL/db1.php
+++ ADMIN/MODEL/db1.php
@@ -17,4 +17,20 @@ function getuserforlogin1($conn, $name, $email) {
     $sql = "SELECT * FROM admin_regi WHERE name='$name' AND email='$email'";
     return $conn->query($sql);
 }
+// Admin profile update-er jonno function
+function updateAdminProfile($conn, $name, $phone, $blood, $file_name, $email, $password = null, $c_password = null) {
+    
+    $email = trim($email);
+    $name = trim($name);
+    
+    if ($password !== null && $password !== "") {
+      
+        $sql = "UPDATE admin_regi SET name='$name', phonenumber='$phone', blood='$blood', profile_pic='$file_name', password='$password', c_password='$c_password' WHERE TRIM(email)='$email'";
+    } else {
+      
+        $sql = "UPDATE admin_regi SET name='$name', phonenumber='$phone', blood='$blood', profile_pic='$file_name' WHERE TRIM(email)='$email'";
+    }
+    
+    return $conn->query($sql);
+}
 ?>
