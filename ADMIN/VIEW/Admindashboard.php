6f1d375dd5a1a288208f959d19e0b6eb7a6329f8
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2026-01-15 06:54:06 +0600

    good

--- ADMIN/VIEW/Admindashboard.php
+++ ADMIN/VIEW/Admindashboard.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["username"])) {
+    header("Location: adminLogin.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,29 +22,29 @@
     <nav class="links"></nav>
         <ul id="navlist">
             <img src="../IMAGES/layout.png" alt="dasboardlogo" id="dashboardlogo">
-           <button type="submit" id="dashboard" onclick="window.location.href='Admindashboard.php'">Dashboard</button>
+           <button type="submit" id="dashboard">Dashboard</button>
 
            <img src="../IMAGES/management.png" alt="managestudentlogo" id="managestudentlogo">
-           <button type="submit" id="manageusers" onclick="window.location.href='ManageUsers.php'">Manage Users</button>
+           <button type="submit" id="manageusers" >Manage Users</button>
 
            <img src="../IMAGES/engineer.png" alt="stafflogo" id="stafflogo">
-           <button type="submit" id="assignstaff" onclick="window.location.href='ManageHostels.php'">Assign staff</button>
+           <button type="submit" id="assignstaff" >Assign staff</button>
 
               <img src="../IMAGES/home-office.png" alt="roombookinglogo" id="roombookinglogo">
-           <button type="submit" id="managebookings" onclick="window.location.href='ManageBookings.php'">Room & Bookings</button>
+           <button type="submit" id="managebookings">Room & Bookings</button>
 
            <img src="../IMAGES/complaint.png" alt="complaintslogo" id="complaintslogo">
-           <button type="submit" id="complaints" onclick="window.location.href='ManageComplaints.php'">Complaints</button>
+           <button type="submit" id="complaints" >Complaints</button>
 
               <img src="../IMAGES/bell.png" alt="noticeslogo" id="noticeslogo">
-           <button type="submit" id="notices" onclick="window.location.href='ManageNotices.php'">Notices</button>
+           <button type="submit" id="notices" >Notices</button>
 
 
                 <img src="../IMAGES/review.png" alt="studentfeedbacklogo" id="studentfeedbacklogo">
-           <button type="submit" id="Student-feedback" onclick="window.location.href='ManageFeedback.php'">Student Feedback</button>
+           <button type="submit" id="Student-feedback" >Student Feedback</button>
 
               <img src="../IMAGES/exit.png" alt="logoutlogo" id="logoutlogo">
-              <button type="submit" id="logout" onclick="window.location.href='Login.php'">Logout</button>
+              <button type="submit" id="logout" onclick="gotologout()">Logout</button>
         </ul>
     </nav>
 
@@ -95,6 +103,7 @@
 
 
 </div>
-    
+     <script src="../JS/gotologout.js"></script>
+ 
 </body>
 </html>
