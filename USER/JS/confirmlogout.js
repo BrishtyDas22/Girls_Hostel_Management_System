ecbcc680dc9052ed996e0d8d916ce22e9e5f4838
pkhunter47 <protikbiswas3099@gmail.com>
2026-01-16 06:00:31 +0600

    wrking

---
+++ USER/JS/confirmlogout.js
@@ -0,0 +1,8 @@
+function handlelogout() {
+            if (confirm("Are you sure you want to Logout?")) {
+                window.location.href = "../CONTROL/logout.php";
+            }
+        }
+
+    
+    
