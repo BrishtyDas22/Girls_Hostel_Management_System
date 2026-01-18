0fbca61b47fe46e79bb3afef1f99808987223727
pkhunter47 <protikbiswas3099@gmail.com>
2026-01-19 00:51:03 +0600

    changes done here too

--- USER/VIEW/notification.php
+++ USER/VIEW/notification.php
@@ -1,21 +1,10 @@
 <?php
 session_start();
-include("../MODEL/db.php");
 
 if (!isset($_SESSION["username"])) {
     header("Location: Login.php");
     exit();
 }
-
-$conn = openConn();
-
-
-$sql = "SELECT notification_id, notification, status FROM notice ORDER BY notification_id DESC";
-$result = $conn->query($sql);
-
-$count_query = "SELECT COUNT(*) as total FROM notice WHERE status='received'";
-$count_res = $conn->query($count_query);
-$count_data = $count_res->fetch_assoc();
 ?>
 
 <!DOCTYPE html>
@@ -42,12 +31,10 @@ $count_data = $count_res->fetch_assoc();
     <a href="notices.php" id="notices"><img src="../images/notice.gif" alt="notice" id="noticeimg">Notices</a>
     
     <a href="notification.php" id="notification">
-        <img src="../images/new-message.gif" alt="notification" id="notificationimg">
-        Notifications
-        <?php if($count_data['total'] > 0): ?>
-            <span class="red-circle"><?= $count_data['total'] ?></span>
-        <?php endif; ?>
-    </a>
+    <img src="../images/new-message.gif" alt="notification" id="notificationimg">
+    Notifications
+    <span id="notif-badge" class="red-circle" style="display:none;"></span>
+</a>
     
     <a href="afterlogin.php" id="booknewroom"><img src="../images/add.gif" alt="newroom" id="booknewroomimg">New Room</a>
     <a href="feedback.php" id="givefeedback"><img src="../images/feedback.gif" alt="feedback" id="feedbackimg">Feedback</a>
@@ -59,27 +46,10 @@ $count_data = $count_res->fetch_assoc();
         <a href="../CONTROL/notificationprocess.php?action=mark_read" class="mark-read-btn">Mark all read</a>
     </div>
     <hr>
-
-    <div class="notification_list">
-        <?php if ($result && $result->num_rows > 0): 
-
-            while($row = $result->fetch_assoc()): ?>
-
-
-            <div class="notif_card <?= ($row['status'] == 'received') ? 'unread' : '' ?>">
-                <div class="notif_icon">
-                    <img src="../images/notice.gif" width="30">
-                </div>
-                <div class="notif_content">
-                    <h4><?= htmlspecialchars($row['notification']) ?></h4>
-                    <p>Status: <p><?= ($row['status']) ?></p></p>
-                </div>
-            </div>
-        <?php endwhile; else: ?>
-            <div id="no-notification">No notifications found.</div>
-        <?php endif; ?>
-    </div>
+    <div id="notif_list_container" class="notification_list"></div>
 </div>
+
+<script src="../JS/AJAX/notification.ajax.js"></script>
 
 </body>
 </html>
