b9edbd3534b798ba7bb69852fe43744488b3b80c
pkhunter47 <protikbiswas3099@gmail.com>
2026-01-19 03:54:30 +0600

    ok

--- USER/VIEW/frontpage1.php
+++ USER/VIEW/frontpage1.php
@@ -36,7 +36,7 @@
 
     <div class="room_info"><h3>Room Info:</h3>
         <p class="room_no">Room No:101 </p>
-    <p class="price">Price: 10000tk/month</p>
+    
 <button type="submit" class="book_btn" onclick="notallowed()">Book Now</button>
 <script src="../JS/alertinbookwithoureg.js"></script>
 
@@ -49,7 +49,7 @@
 
     <div class="room_info1"><h3>Room Info:</h3>
         <p class="room_no">Room No:102 </p>
-    <p class="price">Price: 10000tk/month</p>
+   
 <button type="submit" class="book_btn" onclick="notallowed()">Book Now</button>
 <script src="../JS/alertinbookwithoureg.js"></script>
 
@@ -60,7 +60,7 @@
     <img src="../images/img3.jpg" alt="room image" id="room_image">
     <div class="room_info2"><h3>Room Info:</h3>
         <p class="room_no">Room No:103 </p>
-    <p class="price">Price: 8000tk/month</p>
+ 
 <button type="submit" class="book_btn" onclick="notallowed()">Book Now</button>
 <script src="../JS/alertinbookwithoureg.js"></script>
 
@@ -71,7 +71,7 @@
     <img src="../images/img4.jpg" alt="room image" id="room_image">
     <div class="room_info3"><h3>Room Info:</h3>
         <p class="room_no">Room No:104 </p>
-    <p class="price">Price: 12000tk/month</p>
+    
 <button type="submit" class="book_btn" onclick="notallowed()">Book Now</button>
 <script src="../JS/alertinbookwithoureg.js"></script>
 
@@ -82,7 +82,7 @@
     <img src="../images/img5.png" alt="room image" id="room_image">
     <div class="room_info4"><h3>Room Info:</h3>
         <p class="room_no">Room No:105 </p>
-    <p class="price">Price: 9000tk/month</p>
+    
 <button type="submit" class="book_btn" onclick="notallowed()">Book Now</button>
 <script src="../JS/alertinbookwithoureg.js"></script>
      </div> </div> 
@@ -91,7 +91,7 @@
     <img src="../images/3bed.avif" alt="room image" id="room_image">
     <div class="room_info5"><h3>Room Info:</h3>
         <p class="room_no">Room No:106 </p>
-    <p class="price">Price: 11000tk/month</p>
+    
 <button type="submit" class="book_btn" onclick="notallowed()">Book Now</button>
 <script src="../JS/alertinbookwithoureg.js"></script>
 
