70769b973c051a020cf22162906c8c4e2af11fe7
pkhunter47 <protikbiswas3099@gmail.com>
2026-01-19 00:50:50 +0600

    changes

--- USER/VIEW/notices.php
+++ USER/VIEW/notices.php
@@ -1,21 +1,10 @@
 <?php
 session_start();
-include("../MODEL/db.php");
 
 if (!isset($_SESSION["username"])) {
     header("Location: Login.php");
     exit();
 }
-
-$conn = openConn();
-
-$sql = "SELECT notification_id, notices FROM notice ORDER BY notification_id DESC";
-$result = $conn->query($sql);
-$count_query = "SELECT COUNT(*) as total FROM notice WHERE status='received'";
-$count_res = $conn->query($count_query);
-$count_data = $count_res->fetch_assoc();
-
-
 ?>
 
 <!DOCTYPE html>
@@ -40,59 +29,38 @@ $count_data = $count_res->fetch_assoc();
     <p id="hms">HMS Sidebar</p>
     <a href="dashboard.php" id="overview"><img src="../images/webpage-list.gif" alt="overview" id="overviewimg">Overview</a>
     <a href="complaint.php" id="complaints"><img src="../images/bad-review.gif" alt="complaints" id="complaintsimg">Complaints</a>
-    <a href="notices.php" id="notices" style="background-color: rgb(8, 242, 125); color: white;">
+    <a href="notices.php" id="notices">
         <img src="../images/notice.gif" alt="notice" id="noticeimg">Notices
     </a>
     
-    <a href="notification.php" id="notification">
-        <img src="../images/new-message.gif" alt="notification" id="notificationimg">
-        Notifications
-        <?php if($count_data['total'] > 0): ?>
-            <span class="red-circle"><?= $count_data['total'] ?></span>
-        <?php endif; ?>
-    </a>
+   <a href="notification.php" id="notification">
+    <img src="../images/new-message.gif" alt="notification" id="notificationimg">
+    Notifications
+    <span id="notif-badge" class="red-circle" style="display:none;"></span>
+</a>
     
     <a href="afterlogin.php" id="booknewroom"><img src="../images/add.gif" alt="newroom" id="booknewroomimg">New Room</a>
     <a href="../VIEW/feedback.php" id="givefeedback"><img src="../images/feedback.gif" alt="feedback" id="feedbackimg">Feedback</a>
 </div>
 
 <div id="my_notices">
-    <div class="panel_header">
-        <label>Notices Center</label>
-    </div>
+    <div class="panel_header"><label>Notices Center</label></div>
     <hr>
-
     <table class="notice-table">
         <thead>
             <tr>
                 <th>ID</th>
-                <th>
-                    <center>Description</center>
-                </th>
+                <th><center>Description</center></th>
             </tr>
         </thead>
-        <tbody>
-            <?php if ($result && $result->num_rows > 0): 
-                while($row = $result->fetch_assoc()): ?>
-                <tr>
-                    <td>
-                        <?= $row['notification_id'] ?>
-                    </td>
-                    <td>
-                        <?= htmlspecialchars($row['notices']) ?>
-                    
-                    </td>
-                </tr>
-            <?php endwhile; else: ?>
-                <tr>
-                    <td id="no-notice-text">No notices available at this time.</td>
-                </tr>
-            <?php endif; ?>
-
-
+        <tbody id="notice_table_body">
         </tbody>
+        
     </table>
 </div>
+
+
+<script src="../JS/AJAX/notification.ajax.js"></script>
 
 </body>
 </html>
