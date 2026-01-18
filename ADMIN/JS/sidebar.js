51868b4867ed411c498bb53dc5d3cacfe38c6cda
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2026-01-19 03:16:33 +0600

    d

---
+++ ADMIN/JS/sidebar.js
@@ -0,0 +1,40 @@
+
+    // Dashboard redirection
+    document.getElementById("dashboard").onclick = function() {
+        location.href = "Admindashboard.php";
+    };
+
+    // Room Info redirection
+    document.getElementById("roominfo").onclick = function() {
+        location.href = "room_info.php"; 
+    };
+
+    // Room Bookings redirection
+    document.getElementById("managebookings").onclick = function() {
+        location.href = "room_bookings.php";
+    };
+
+    // Manage Students redirection
+    document.getElementById("manageusers").onclick = function() {
+        location.href = "manage_students.php";
+    };
+
+    // Complaints redirection
+    document.getElementById("complaints").onclick = function() {
+        location.href = "manage_complain.php";
+    };
+
+    // Notices redirection
+    document.getElementById("notices").onclick = function() {
+        location.href = "managenotice.php";
+    };
+
+    // Student Feedback redirection
+    document.getElementById("Student-feedback").onclick = function() {
+        location.href = "manage_feedback.php";
+    };
+
+    // Assign Staff redirection
+    document.getElementById("assignstaff").onclick = function() {
+        location.href = "assign_staff.php";
+    };
