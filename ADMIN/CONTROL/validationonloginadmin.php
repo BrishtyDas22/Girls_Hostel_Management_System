87aefc0da8006741bad68c413a60332d0ee25a17
BrishtyDas22 <210949928+BrishtyDas22@users.noreply.github.com>
2026-01-19 16:46:48 +0600

    done

--- ADMIN/CONTROL/validationonloginadmin.php
+++ ADMIN/CONTROL/validationonloginadmin.php
@@ -1,9 +1,19 @@
 <?php
 include '../MODEL/db1.php';
+//  Prothome Cookie theke value check kora
+$stored_name = isset($_COOKIE['remember_name']) ? $_COOKIE['remember_name'] : "";
+$stored_email = isset($_COOKIE['remember_email']) ? $_COOKIE['remember_email'] : "";
+//Initialize variables
 $name="";
 $email="";
 $password="";
-
+// Jodi cookie thake, tobe variable e cookie-r value set kora
+if (!empty($stored_name)) {
+    $name = $stored_name;
+}
+if (!empty($stored_email)) {
+    $email = $stored_email;
+}
 $success_msg="";
 
 $name_error="";
@@ -51,7 +61,17 @@ if ($result->num_rows > 0) {
         $_SESSION["phonenumber"] = $row['phonenumber'];
         $_SESSION["blood"]       = $row['blood'];
         $_SESSION["profile_pic"] = $row['profile_pic'];
-    
+    //  cookie set korar code  
+                if (isset($_POST['remember'])) {
+                    // 30 diner jonno name ar email save korlam
+                    setcookie("remember_name", $name, time() + (86400 * 30), "/", "", false, true);
+                    setcookie("remember_email", $email, time() + (86400 * 30), "/", "", false, true);
+                } else {
+                    // User jodi checkbox click na kore, purono cookie thakle delete kore dao
+                    setcookie("remember_name", "", time() - 3600, "/");
+                    setcookie("remember_email", "", time() - 3600, "/");
+                }
+                // ------------------------------
            $success_msg="Login successful!";
 
     } else {
